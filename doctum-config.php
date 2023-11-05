f56dda66f52e6a1b8186e6dba3b4a9b0255dfd42
Joe <104938042+lrljoe@users.noreply.github.com>
2023-11-05 21:54:02 +0000

    Awful approach

--- doctum-config.php
+++ doctum-config.php
@@ -5,7 +5,7 @@ use Doctum\RemoteRepository\GitHubRemoteRepository;
 use Doctum\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
-$dir = './src';
+$dir = '.';
 
 $iterator = Finder::create()
     ->files()
@@ -18,17 +18,16 @@ $iterator = Finder::create()
 $versions = GitVersionCollection::create($dir)
     // In a non case-sensitive way, tags containing "PR", "RC", "BETA" and "ALPHA" will be filtered out
     // To change this, use: `$versions->setFilter(static function (string $version): bool { // ... });`
-    ->add('doctum-test', 'doctum-test branch')
-    ->addFromTags('v3.*')
-    ->addFromTags('v2.*')
-    ->addFromTags('v1.*');
+    ->add('master', '3.x branch')
+    ->add('v2-master', '2.x branch')
+    ->add('v1', '1.x branch');
 
 return new Doctum($iterator, [
-    'versions' => $versions,
-    'title' => 'Rappasoft - Laravel Livewire Tables API',
-    'build_dir' => __DIR__.'/doctum/build/%version%',
-    'cache_dir' => __DIR__.'/doctum/cache/%version%',
-    'source_dir' => dirname($dir).'/',
-    'remote_repository' => new GitHubRemoteRepository('rappasoft/laravel-livewire-tables', dirname($dir)),
+    'versions'             => $versions,
+    'title'                => 'Rappasoft - Laravel Livewire Tables API',
+    'build_dir'            => __DIR__ . '/build/%version%',
+    'cache_dir'            => __DIR__ . '/cache/%version%',
+    'source_dir'           => dirname($dir) . '/',
+    'remote_repository'    => new GitHubRemoteRepository('rappasoft/laravel-livewire-tables', dirname($dir)),
     'default_opened_level' => 2, // optional, 2 is the default value
 ]);
